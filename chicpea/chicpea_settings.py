72b356eab175f6699a0317596eceb419c7d4f671
tottlefields <ellen.schofield@cimr.cam.ac.uk>
2015-05-21 09:40:22 +0100

    chicpea settings file added

---
+++ chicpea/chicpea_settings.py
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+
+CHICP_IDX = OrderedDict([
+    ('barrett', {'NAME': 'T1D - Barrett et al.', 'INDEX': 'cp:hg19_t1d_barrett_bed'}),
+    ('cooper', {'NAME': 'T1D - Cooper NJ', 'INDEX': 'cp:hg19_t1d_cooper_bed'}),
+    ('onengut', {'NAME': 'T1D - Onengut et al.', 'INDEX': 'cp:hg19_t1d_onengut_bed'}),
+    ('ob-pmi_bp_diastolic', {'NAME': 'Diastolic Blood Pressure', 'INDEX': 'cp:hg19_ob-pmi_bp_diastolic'}),
+    ('ob-pmi_bp_systolic', {'NAME': 'Systolic Blood Pressure', 'INDEX': 'cp:hg19_ob-pmi_bp_systolic'}),
+    ('ob-pmi_cel', {'NAME': 'Celiac Disease', 'INDEX': 'cp:hg19_ob-pmi_cel'}),
+    ('ob-pmi_cro', {'NAME': "Crohn's Disease", 'INDEX': 'cp:hg19_ob-pmi_cro'}),
+    ('ob-pmi_hb', {'NAME': "Haemoglobin", 'INDEX': 'cp:hg19_ob-pmi_hb'}),
+    ('ob-pmi_height', {'NAME': "Height", 'INDEX': 'cp:hg19_ob-pmi_height'}),
+    ('ob-pmi_mchc', {'NAME': "Mean Corpuscular Haemoglobin Concentration", 'INDEX': 'cp:hg19_ob-pmi_mchc'}),
+    ('ob-pmi_mch', {'NAME': "Mean corpuscular Haemoglobin", 'INDEX': 'cp:hg19_ob-pmi_mch'}),
+    ('ob-pmi_mcv', {'NAME': " Mean Corpuscular Volume", 'INDEX': 'cp:hg19_ob-pmi_mcv'}),
+    ('ob-pmi_ms', {'NAME': "Multiple Sclerosis", 'INDEX': 'cp:hg19_ob-pmi_ms'}),
+    ('ob-pmi_pcv', {'NAME': "Packed Cell Volume/Haematocrit", 'INDEX': 'cp:hg19_ob-pmi_pcv'}),
+    ('ob-pmi_plt', {'NAME': "Platelet Count", 'INDEX': 'cp:hg19_ob-pmi_plt'}),
+    ('ob-pmi_pv', {'NAME': "Platelet Volume", 'INDEX': 'cp:hg19_ob-pmi_pv'}),
+    ('ob-pmi_ra', {'NAME': "Rheumatoid Arthritis", 'INDEX': 'cp:hg19_ob-pmi_ra'}),
+    ('ob-pmi_rbc', {'NAME': "Red Blood Cells", 'INDEX': 'cp:hg19_ob-pmi_rbc'}),
+    ('ob-pmi_t1d', {'NAME': "Type 1 Diabetes", 'INDEX': 'cp:hg19_ob-pmi_t1d'}),
+    ('ob-pmi_t2d', {'NAME': "Type 2 Diabetes", 'INDEX': 'cp:hg19_ob-pmi_t2d'}),
+    ('ob-pmi_uc', {'NAME': "Ulcerative Colitis", 'INDEX': 'cp:hg19_ob-pmi_uc'})
+])
