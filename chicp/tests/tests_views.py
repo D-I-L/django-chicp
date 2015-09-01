86d94edad731e1c7a6072865b3f0348b1557a8f3
tottlefields <ellen.schofield@cimr.cam.ac.uk>
2015-09-01 16:01:44 +0100

    renamed chicpea to chicp

--- chicp/tests/tests_views.py
+++ chicp/tests/tests_views.py
@@ -18,22 +18,22 @@ class ChicpeaTestCase(TestCase):
             utils.tissues[idx] = tissueList
 
     def test_page(self):
-        ''' Test the main chicpea page. '''
-        found = resolve('/chicpea/')
+        ''' Test the main CHiCP page. '''
+        found = resolve('/chicp/')
         self.assertEqual(found.func, chicpea)
 
     def test_page_returns_correct_html(self):
-        ''' Test the content returned for the main chicpea page. '''
+        ''' Test the content returned for the main CHiCP page. '''
         request = self.factory.get('/')
         response = chicpea(request)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'IL2RA', response.content)
 
     def test_page_with_hgnc(self):
-        ''' Test chicpea page with HGNC Gene name '''
-        response = self.client.get(reverse('chicpea:chicpea') + '?term=DEXI')
+        ''' Test CHiCP page with HGNC Gene name '''
+        response = self.client.get(reverse('chicp:chicp') + '?term=DEXI')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'chicpea/index.html')
+        self.assertTemplateUsed(response, 'chicp/index.html')
         self.assertIn(b'DEXI', response.content)
         for idx in utils.tissues:
             tissueList = utils.tissues[idx]
@@ -41,47 +41,47 @@ class ChicpeaTestCase(TestCase):
                 self.assertIn(t.replace("_", " ").encode('ascii'), response.content)
 
     def test_page_with_ensg(self):
-        ''' Test chicpea page with EnsEMBL Gene '''
-        response = self.client.get(reverse('chicpea:chicpea') + '?term=ENSG00000182108')
+        ''' Test CHiCP page with EnsEMBL Gene '''
+        response = self.client.get(reverse('chicp:chicp') + '?term=ENSG00000182108')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'chicpea/index.html')
+        self.assertTemplateUsed(response, 'chicp/index.html')
         self.assertIn(b'ENSG00000182108', response.content)
 
     def test_page_with_geneerror(self):
-        ''' Test chicpea page with EnsEMBL Gene '''
+        ''' Test CHiCP page with EnsEMBL Gene '''
         geneName = 'ENSG00000'
-        response = self.client.get(reverse('chicpea:chicpea') + '?term='+geneName)
+        response = self.client.get(reverse('chicp:chicp') + '?term='+geneName)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'chicpea/index.html')
+        self.assertTemplateUsed(response, 'chicp/index.html')
 
     def test_search(self):
-        ''' Test the chicpea search. '''
-        found = resolve('/chicpea/search/')
+        ''' Test the CHiCP search. '''
+        found = resolve('/chicp/search/')
         self.assertEqual(found.func, chicpeaSearch)
 
     def test_chicpeasearch_gene(self):
-        ''' Test chicpea search for a gene & tissue '''
+        ''' Test CHiCP search for a gene & tissue '''
         geneName = 'IL2RA'
         targetIdx = 'cp:hg19_mifsud_gt_pm'
-        url = "/chicpea/search?searchTerm="+geneName+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
+        url = "/chicp/search?searchTerm="+geneName+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
         request = self.factory.get(url)
         response = chicpeaSearch(request, url)
         self._HICtest(str(response.content, encoding='utf8'))
 
     def test_chicpeasearch_region(self):
-        ''' Test chicpea search for a region '''
+        ''' Test CHiCP search for a region '''
         region = 'X:153043000-153390000'
         targetIdx = 'cp:hg19_mifsud_gt_pm'
-        url = "/chicpea/search?searchTerm="+region+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
+        url = "/chicp/search?searchTerm="+region+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
         request = self.factory.get(url)
         response = chicpeaSearch(request, url)
         self._HICtest(str(response.content, encoding='utf8'))
 
     def test_chicpeasearch_snp(self):
-        ''' Test chicpea search for a gene & tissue '''
+        ''' Test CHiCP search for a gene & tissue '''
         snp = 'rs2476601'
         targetIdx = 'cp:hg19_mifsud_gt_pm'
-        url = "/chicpea/search?searchTerm="+snp+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
+        url = "/chicp/search?searchTerm="+snp+"&targetIdx="+targetIdx+"&snp_track=gwas-barrett"
         request = self.factory.get(url)
         response = chicpeaSearch(request, url)
         self._HICtest(str(response.content, encoding='utf8'))
