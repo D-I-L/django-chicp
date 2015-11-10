fcc4df8192f34a6b740fc60d8a52f0270bd4c619
tottlefields <ellen.schofield@cimr.cam.ac.uk>
2015-11-10 16:03:51 +0000

    added a url rule for contact email

--- chicp/urls.py
+++ chicp/urls.py
@@ -4,6 +4,7 @@ from chicp import views
 urlpatterns = patterns('',
                        url(r'^$', views.chicpea, name='chicp'),
                        url(r'^docs$', views.chicpeaDocs, name='chicpeaDocs'),
+                       url(r'^contact$', views.contactUs, name='contactUs'),
                        url(r'^search(.*)$', views.chicpeaSearch, name='chicpeaSearch'),
                        url(r'^subSearch(.*)$', views.chicpeaSubSearch, name='chicpeaSubSearch'),
                        url(r'^fileUpload(.*)$', views.chicpeaFileUpload, name='chicpeaFileUpload'),
