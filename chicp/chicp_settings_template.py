1d5edcf2111d619d549c75782577f9f389e455de
tottlefields <ellen.schofield@cimr.cam.ac.uk>
2015-11-11 14:46:51 +0000

    removed extra info from template settings file.

--- chicp/chicp_settings_template.py
+++ chicp/chicp_settings_template.py
@@ -1,5 +1,5 @@
-CP_STATS = ['UD', 'IC', 'GWAS']
-CP_TARGET = ['CHICAGO', 'MIFSUD', 'MARTIN']
+CP_STATS = ['UD', 'GWAS']
+CP_TARGET = ['MIFSUD']
 
 sampleLookup = {}
 
@@ -14,36 +14,3 @@ DEFAULT_GENES = ['IL2RA', 'DEXI', 'BACH2', 'FOXP3', 'DOCK1']
 
 STUDY_DEFAULTS = {'log10p': {'min': 1, 'snpCutoff': 7.03, 'score_text': "P Value (-log10)"},
                   'ppi': {'min': 0.001, 'snpCutoff': 0.1, 'score_text': "PPI Score"}}
-
-ELASTIC = {
-    'default': {
-        'ELASTIC_URL': ['http://127.0.0.1:9200/'],
-        'IDX': {
-            'CP_STATS_UD': {
-                'name': '',
-                'label': 'User Data',
-                'idx_type': {},
-                'data_type': 'log10p',
-                },
-            'CP_STATS_IC': {
-                'name': '',
-                'label': 'ImmunoChip',
-                'idx_type': {
-                },
-                'data_type': 'log10p',
-            },
-            'CP_STATS_GWAS': {
-                'name': '',
-                'label': 'GWAS Statistic',
-                'idx_type': {
-                },
-                'data_type': 'log10p',
-            },
-            'CP_TARGET_CHICAGO': {},
-            'CP_TARGET_MIFSUD': {},
-            'CP_TARGET_MARTIN': {},
-        },
-        'TEST': 'auto_tests',
-        'REPOSITORY': 'my_backup',
-    }
-}
